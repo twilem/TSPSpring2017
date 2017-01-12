5a015d7112b23399de143a57b890ebfe03cfd1ba
brkurzawa <brkurzawa@gmail.com>
2017-01-12 13:08:24 -0500

    Added name to participants.c

--- code/participants.c
+++ code/participants.c
@@ -33,6 +33,8 @@ main (int argc, char *argv[])
   
   printf ("%02d: Kavan Ferguson\n",++i);
 
+  printf ("%02d: Brett Kurzawa\n", ++i);
+
   printf ("There are %i participants.\n", i);
   return 0;
 } // main
